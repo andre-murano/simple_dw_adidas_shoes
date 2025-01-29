3302e50b6aee0d4d82f731d17f4fd730b24afe82
André Murano <andre.murano@hotmail.com>
2025-01-28 21:41:09 -0300

    Criação do data_unzip

---
+++ src/data_unzip.py
@@ -0,0 +1,25 @@
+import zipfile
+import os
+
+def descompactar_arquivo(zip_path):
+
+    destino = os.path.dirname(zip_path)  # Define o mesmo diretório do ZIP
+
+    try:
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(destino)
+        print(f"Arquivo descompactado em: {destino}")
+    except zipfile.BadZipFile:
+        print("Erro: O arquivo ZIP está corrompido ou inválido.")
+    except FileNotFoundError:
+        print("Erro: Arquivo ZIP não encontrado.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
+
+# Caminho do arquivo ZIP
+zip_file = r"C:\Users\andre\Desktop\Projetos\projeto1\data\dataset\archive.zip"
+
+# Chamando a função
+descompactar_arquivo(zip_file)
+        
+
